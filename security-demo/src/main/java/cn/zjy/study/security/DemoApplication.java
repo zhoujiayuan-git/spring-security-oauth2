479877ff0ab734fc87f35c911054f23ea951b8bb
周佳媛 <1064078707@qq.com>
2021-03-08 16:49:20 +0800

    在demo中创建hello spring security

--- security-demo/src/main/java/cn/zjy/study/security/DemoApplication.java
+++ security-demo/src/main/java/cn/zjy/study/security/DemoApplication.java
@@ -1,4 +1,26 @@
 package cn.zjy.study.security;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class DemoApplication {
+
+    /**
+     * @Description
+     * @Author  zjy
+     * @Date   2021/3/8 16:09
+     * @param  args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring security";
+    }
 }
